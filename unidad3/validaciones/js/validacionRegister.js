751f6ec6fca75c317ab05faeece1e5a76cae36f0
Angel Cordova <angel565660@outlook.es>
2023-10-12 23:39:20 -0600

    Test

---
+++ unidad3/validaciones/js/validacionRegister.js
@@ -0,0 +1,10 @@
+window.onload() = () => {
+    var alert = document.querySelector("#alert");
+    var alertSuccess = document.querySelector("#alertSuccess");
+    var btn = document.querySelector("#btn");
+    
+    btn.addEventListener('click', (evt) => {
+        
+
+    })
+}
