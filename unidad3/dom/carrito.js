0ac3418e7ddba347bfcaaa6dc90fd79a651ef0d3
AnGeL505Xbox <angel565660@outlook.es>
2023-10-18 13:42:26 -0600

    Dom 2

---
+++ unidad3/dom/carrito.js
@@ -0,0 +1,9 @@
+window.onload = () =>{    
+    var data = [
+        { id:1, name:"Chillorio", price:50.00, img:"", },
+        { id:2, name:"Enchilada", price:75.00, img:"", },
+        { id:3, name:"Tortilla española", price:60.00, img:"", },
+        { id:4, name:"Tamal", price:15.00, img:"", },
+        { id:5, name:"Omelet", price:80.00, img:"", }
+    ]
+}
