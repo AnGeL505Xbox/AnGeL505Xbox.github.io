b4acb5ced7417ca083f8244f3e63cc5b3a89c3a5
AnGeL505Xbox <angel565660@outlook.es>
2023-10-18 13:24:49 -0600

    Test

--- unidad3/validaciones/js/validacionRegister.js
+++ unidad3/validaciones/js/validacionRegister.js
@@ -1,10 +1,29 @@
-window.onload() = () => {
+window.onload = () => {
     var alert = document.querySelector("#alert");
     var alertSuccess = document.querySelector("#alertSuccess");
     var btn = document.querySelector("#btn");
+    var txName = document.querySelector("#txName")
+    var txTel = document.querySelector("#txTel")
+    var txAge = document.querySelector("#txAge")
+    var txPass = document.querySelector("#txPass")
+    var txPassConf = document.querySelector("#txPassConf")
+
     
     btn.addEventListener('click', (evt) => {
-        
-
+        evt.preventDefault();
+        if ((txName.value == "")||(txTel.value == "")||(txAge.value == "")||(txPass.value == "")||(txPassConf.value == "")){
+            alert.innerHTML = 'Error: <br> <small>Favor de llenar los campos</small>';
+            alert.style.display = "block";
+        }
+        else {
+            if (txPass.value == txPassConf.value){
+                alertSuccess.innerHTML = 'Error: <br> <small>La contraseña debe de tener mas de 5 letras</small>';
+                alertSuccess.style.display = "block";
+            }else
+            {
+                alert.innerHTML = 'Error: <br> <small>Las contraseñas no coiciden</small>';
+                alert.style.display = "block";
+            }
+        }
     })
 }
